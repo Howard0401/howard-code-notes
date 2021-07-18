d50430497e533888e29e27663216ebf5c430810b
howard chen <chenhoward0401@gmail.com>
2021-07-19 04:18:21 +0800

    feat:[Howard] backup

--- deploy.sh
+++ deploy.sh
@@ -13,7 +13,10 @@ setProjectName() {
 
 # scripts
 set -e
+rm -rf ./public
 npm run build
 setProjectName
 git add public && git commit -m "init web"
+# https://stackoverflow.com/questions/33172857/how-do-i-force-a-subtree-push-to-overwrite-remote-changes
+git push origin `git subtree push --prefix public origin gh-pages` :gh-pages --force
 git subtree push --prefix public origin gh-pages
