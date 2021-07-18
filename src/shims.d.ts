1e8668acd928d37e797d15bcfccbe25b16db5859
howard chen <chenhoward0401@gmail.com>
2021-07-19 03:31:31 +0800

    init web

--- src/shims.d.ts
+++
@@ -1,6 +0,0 @@
-declare module '@kangc/v-md-editor'
-declare module '@kangc/v-md-editor/lib/preview'
-// declare module '@kangc/v-md-editor/lib/theme/vuepress.js';
-declare module '@kangc/v-md-editor/lib/theme/github.js'
-declare module 'codemirror'
-declare module 'vue3-markdown-it'
